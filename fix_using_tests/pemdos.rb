fc0f7c73a147e7c60209039037d3fe9c2c2f9382
Olesya Miller <olesyam.miller@gmail.com>
2019-12-04 19:17:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,7 @@
 def snake_it_up(string)
     #binding.pry 
-  my_string = "s"
   if string[0] == "s"
-    return my_string * 10  + string
+    return my_string * "s"  + string
   else
     return string
   end
